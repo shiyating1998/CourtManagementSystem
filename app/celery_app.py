bf9eb967364d12842c05e0cdee2963476b9719b9
Jennifer Shi <shiyating1998@gmail.com>
2024-12-22 22:31:58 -0500

    fix celery_app

--- app/celery_app.py
+++ app/celery_app.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 import eventlet
-impot ssl 
+import ssl 
 eventlet.monkey_patch()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'courtManagementSystem.settings')
